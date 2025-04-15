1b427824792b2a40d13cbb9b11b976442c548fae
MDATIK-3 <atikh4335@gmail.com>
2025-04-15 13:32:03 +0600

    update layout.jsx: modify HTML structure, add meta tags and script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,21 +4,23 @@ import { Analytics } from "@vercel/analytics/react";
 import FeedbackButton from "@/app/component/FeedbackButton/page";
 import Header from "@/app/layout/Header";
 import Footer from "@/app/layout/Footer";
-import Head from "next/head";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>CipherAlgo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/images/logo1.png" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5385144928798539"
+          crossorigin="anonymous"></script>
+      </head>
+
       <body className="flex flex-col min-h-screen">
-        <Head>
-          <link rel="icon" href="/images/logo1.png" />
-        </Head>
-
         <Header />
-
         <main className="flex-grow">{children}</main>
-
         <FeedbackButton />
         <Analytics />
         <Footer />
